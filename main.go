0894ab450baa2b8314ae7d26928929e4ee3f2e37
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2025-03-30 15:38:43 +0530

    Implement basic http request handler

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main(){
+	fmt.Println("Hello123")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/user/:id", func(c *gin.Context) {
+		userID := c.Param("id") // Extracts `id` from URL path
+
+		// JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"user_id": userID,
+			"message": "User found",
+		})
+	})
+
+	router.Run()
+}
